72b3c27800cd388b822316e5a68b4e8012b75db3
KingDeng005 <fuheng.deng@tusimple.ai>
2017-10-14 18:37:39 -0700

    update format - tab equals 4 spaces

--- src/AutolivNode.cpp
+++ src/AutolivNode.cpp
@@ -9,14 +9,14 @@ AutolivNode::~AutolivNode(){
 AutolivNode::AutolivNode(ros::NodeHandle &node, ros::NodeHandle &priv_nh){
 
     // publisher
-	pub_target_polar_short_ = node.advertise<TargetPolarShort>('target_polar_short', 100);
-	pub_raw_polar_short_ = node.advertise<RawPolarShort>('raw_polar_short', 2);
-	pub_target_cartesian_ = node.advertise<TargetCartesian>('target_cartesian', 2);
-	pub_target_cartesian_mid = node.advertise<TargetCartesianMid>('target_cartesian_mid', 2);
-	pub_target_cartesian_mul = node.advertise<TargetCartesianMul>('target_cartesian_mul', 2);
-	pub_freespace_segments = node.advertise<FreespaceSegments>('freespace_segments', 2);
-	pub_raw_polar_long = node.advertise<RawPolarLong>('raw_polar_long', 2);
-	pub_target_polar_long = node.advertise<TargetPolarLong>('target_polar_long', 2);
+    pub_target_polar_short_ = node.advertise<TargetPolarShort>('target_polar_short', 100);
+    pub_raw_polar_short_ = node.advertise<RawPolarShort>('raw_polar_short', 2);
+    pub_target_cartesian_ = node.advertise<TargetCartesian>('target_cartesian', 2);
+    pub_target_cartesian_mid = node.advertise<TargetCartesianMid>('target_cartesian_mid', 2);
+    pub_target_cartesian_mul = node.advertise<TargetCartesianMul>('target_cartesian_mul', 2);
+    pub_freespace_segments = node.advertise<FreespaceSegments>('freespace_segments', 2);
+    pub_raw_polar_long = node.advertise<RawPolarLong>('raw_polar_long', 2);
+    pub_target_polar_long = node.advertise<TargetPolarLong>('target_polar_long', 2);
 
     // subscriber
     sub_can_ = node.subscribe("/can_tx", 1, &AutolivNode::recvCAN,this);
@@ -24,8 +24,8 @@ AutolivNode::AutolivNode(ros::NodeHandle &node, ros::NodeHandle &priv_nh){
 }
 
 void procTargetPolarShort(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
-	const TargetPolarShort *ptr = (const TargetPolarShort*)msg->msg.data.elems;
-	
+    const TargetPolarShort *ptr = (const TargetPolarShort*)msg->msg.data.elems;
+    
 }
 
 void procRawPolarShort(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
@@ -56,5 +56,5 @@ void procTargetPolarLong(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &
 
 }
 
-	
+    
 }
--- src/AutolivNode.h
+++ src/AutolivNode.h
@@ -16,34 +16,34 @@
 namespace octopus{
 class AutolivNode{
 public:
-	AutolivNode(ros::NodeHandle &node, ros::NodeHandle &priv_nh);
-	~AutolivNode();
+    AutolivNode(ros::NodeHandle &node, ros::NodeHandle &priv_nh);
+    ~AutolivNode();
 
 private:
 
-	// functions to receive and process the can messages
-	void recvCAN(const dataspeed_can_msgs::CamMessagesStamped::ConstPtr &msg);
-	void procTargetPolarShort(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
-	void procRawPolarShort(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
-	void procTargetCartesian(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
-	void procTargetCartesianMid(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
-	void procTargetCartesianMul(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
-	void procFreespaceSegments(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
-	void procRawPolarLong(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
-	void procTargetPolarLong(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
+    // functions to receive and process the can messages
+    void recvCAN(const dataspeed_can_msgs::CamMessagesStamped::ConstPtr &msg);
+    void procTargetPolarShort(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
+    void procRawPolarShort(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
+    void procTargetCartesian(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
+    void procTargetCartesianMid(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
+    void procTargetCartesianMul(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
+    void procFreespaceSegments(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
+    void procRawPolarLong(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
+    void procTargetPolarLong(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg);
 
-	// subscriber
-	ros::Subscriber sub_can_;
+    // subscriber
+    ros::Subscriber sub_can_;
 
-	// publiser
-	ros::Publisher pub_target_polar_short_;
-	ros::Publisher pub_raw_polar_short_;
-	ros::Publisher pub_target_cartesian_;
-	ros::Publisher pub_target_cartesian_mid_;
-	ros::Publisher pub_target_cartesian_mul_;
-	ros::Publisher pub_freespace_segments_;
-	ros::Publisher pub_raw_polar_long_;
-	ros::Publisher pub_target_polar_long_;
+    // publiser
+    ros::Publisher pub_target_polar_short_;
+    ros::Publisher pub_raw_polar_short_;
+    ros::Publisher pub_target_cartesian_;
+    ros::Publisher pub_target_cartesian_mid_;
+    ros::Publisher pub_target_cartesian_mul_;
+    ros::Publisher pub_freespace_segments_;
+    ros::Publisher pub_raw_polar_long_;
+    ros::Publisher pub_target_polar_long_;
 
 };
 }
