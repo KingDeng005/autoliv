f99fda0a6bb3fd59ae49b2d0333cc85fe8e834d3
kingdeng05 <kingdeng05@foxmail.com>
2017-10-15 02:51:57 -0700

    1. add content in Node.cpp; 2. modify msg; 3. modify dispatch

--- src/AutolivNode.cpp
+++ src/AutolivNode.cpp
@@ -3,9 +3,10 @@
 
 namespace octopus
 {
-AutolivNode::~AutolivNode(){
 
-}
+// this inline function is to transform uint16t data into int16_t
+int16_t inline uint2int(uint16_t data, int bit_num) {return ((int16_t)(data<<(16-bit_num)))>>bit_num;}
+
 AutolivNode::AutolivNode(ros::NodeHandle &node, ros::NodeHandle &priv_nh){
 
     // publisher
@@ -19,42 +20,128 @@ AutolivNode::AutolivNode(ros::NodeHandle &node, ros::NodeHandle &priv_nh){
     pub_target_polar_long = node.advertise<TargetPolarLong>('target_polar_long', 2);
 
     // subscriber
-    sub_can_ = node.subscribe("/can_tx", 1, &AutolivNode::recvCAN,this);
+    sub_can_ = node.subscribe("/can_tx", 1, &AutolivNode::recvCAN, this);
 
 }
 
-void procTargetPolarShort(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
-    const TargetPolarShort *ptr = (const TargetPolarShort*)msg->msg.data.elems;
-    
+AutolivNode::~AutolivNode(){
 }
 
-void procRawPolarShort(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
-
+// this function is to get the target format type of a message
+int AutolivNode::getTargetType(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
+    const MsgTargetGeneral *ptr = (const MsgTargetGeneral*)msg->msg.data.elems;
+    return ptr->type;
 }
 
-void procTargetCartesian(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
+void AutolivNode::recvCAN(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
 
+    // deal with target message for now
+    if(msg->msg.id > 0x300 && msg->msg.id < 0x309){
+        switch(getTargetType(msg)){
+            case ID_TARGET_POLAR_SHORT:
+                procTargetPolarShort(msg);
+            case ID_RAW_POLAR_SHORT:
+                procRawPolarShort(msg);
+            case ID_TARGET_CARTESIAN:
+                procTargetCartesian(msg);
+            case ID_TARGET_CARTESIAN_MID:
+                procTargetCartesianMid(msg);
+            case ID_TARGET_CARTESIAN_MUL:
+                procTargetCartesianMul(msg);
+            case ID_FREESPACE_SEGMENTS:
+                procFreespaceSegments(msg);
+            case ID_RAW_POLAR_LONG:
+                procRawPolarLong(msg);
+            case ID_TARGET_POLAR_LONG:
+                procTargetPolarLong(msg);
+            default:
+                ROS_DEBUG("undefined CAN id!");
+            break;
+        }
+        
+    }
 }
 
-void procTargetCartesianMid(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
+void AutolivNode::procTargetPolarShort(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
+    const MsgTargetPolarShort *ptr = (const MsgTargetPolarShort*)msg->msg.data.elems;
+    float range = (float)(ptr->range_msb << 4 + ptr->range_lsb) / 100;
+    float velocity = (float)uint2int(ptr->velocity_msb << 6 + ptr->velocity_lsb, 10) / 10 - 20;
+    float bearing = (float)uint2int(ptr->bearing_msb << 8 + ptr->bearing_lsb, 10) / 5;
+    uint8_t quality = ptr->quality;
+    uint8_t track_id = ptr->track_id;
+    uint8_t msg_counter = ptr->msg_counter;
+    uint8_t sensor_nr = ptr->sensor_nr;
+    uint8_t type = ptr->target_format_type;
+    int16_t bearing_obs = uint2int(ptr->bearing_observed_msb << 2 + ptr->bearing_observed_lsb, 6);
+    float range_obs = (float)uint2int(ptr->range_observed, 6) / 100;
 
+    // fill message and publish
+    TargetPolarShort out;
+    out.header.frame_id = "base_link";
+    out.header.stamp = ros::Time::now();
+    out.range = range;
+    out.velocity = velocity;
+    out.bearing = bearing;
+    out.quality = quality;
+    out.track_id = track_id;
+    out.msg_counter = msg_counter;
+    out.sensor_nr = sensor_nr;
+    out.target_format_type = target_format_type;
+    out.bearing_raw = bearing_obs;
+    out.range_raw = range_obs;
+    pub_target_polar_short_.publish(out);
 }
 
-void procTargetCartesianMul(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
+void AutolivNode::procRawPolarShort(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
+    const MsgRawPolarShort *ptr = (const MsgRawPolarShort*)msg->msg.data.elems;
+    float range = (float)(ptr->range_msb << 4 + ptr->range_lsb) / 100;
+    float doppler_vel = (float)uint2int(ptr->velocity_msb << 6 + ptr->velocity_lsb, 10) / 10 - 20;
+    float bearing = (float)uint2int(ptr->bearing_msb << 8 + ptr->bearing_lsb, 10) / 5; 
+    float amp = (float)ptr->amplitude / 2;
+    uint8_t msg_counter = ptr->msg_counter;
+    uint8_t sensor_nr = ptr->sensor_nr;
+    uint8_t type = ptr->target_format_type;
+    uint8_t usage = ptr->usage;
+    float doppler_alias = (float)ptr->doppler_alias / 5;
 
+    // fill message and publish
+    RawPolarShort out;
+    out.header.frame_id = "base_link";
+    out.header.stamp = ros::Time::now();
+    out.range = range;
+    out.doppler_velocity = doppler_velocity;
+    out.bearing = bearing;
+    out.amplitude = amp;
+    out.msg_counter = msg_counter;
+    out.sensor_nr = sensor_nr;
+    out.target_format_type = target_format_type;
+    out.usage = usage;
+    out.doppler_alias = doppler_alias;
+    pub_raw_polar_short_.publish(out);
 }
 
-void procFreespaceSegments(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
-
+void AutolivNode::procTargetCartesian(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
+    // TODO
 }
 
-void procRawPolarLong(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
-
+void AutolivNode::procTargetCartesianMid(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
+    // TODO
 }
 
-void procTargetPolarLong(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
-
+void AutolivNode::procTargetCartesianMul(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
+    // TODO
 }
 
+void AutolivNode::procFreespaceSegments(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
+    // TODO
+}
+
+void AutolivNode::procRawPolarLong(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
+    // TODO
+}
+
+void AutolivNode::procTargetPolarLong(const dataspeed_can_msgs::CanMessageStamped::ConstPtr &msg){
+    // TODO
+}
     
 }
--- src/dispatch.h
+++ src/dispatch.h
@@ -4,21 +4,35 @@
 
 namespace octopus
 {
+
+typedef struct
+{
+  uint8_t : 8;
+  uint8_t : 8;
+  uint8_t : 8;
+  uint8_t : 8;
+  uint8_t : 8;
+  uint8_t : 8;
+  uint8_t type : 4;
+  uint8_t : 4;
+  uint8_t : 8;
+} MsgTargetGeneral
+
 typedef struct
 {
   uint8_t range_msb;
   uint8_t range_lsb : 4;
   uint8_t velocity_msb : 4;
   uint8_t velocity_lsb : 6;
-  uint8_t bearing_filtered_msb : 2;
-  uint8_t bearing_filtered_lsb : 8;
+  uint8_t bearing_msb : 2;
+  uint8_t bearing_lsb : 8;
   uint8_t quality : 4;
   uint8_t track_id : 4;
   uint8_t msg_counter : 4;
   uint8_t sensor_nr : 4;
   uint8_t target_format_type : 4;
   uint8_t bearing_observed_msb : 4;
-  uint8_t bearing_observed_msb : 2;
+  uint8_t bearing_observed_lsb : 2;
   uint8_t range_observed : 6;
 } MsgTargetPolarShort;
 
@@ -27,7 +41,7 @@ typedef struct
   uint8_t range_msb;
   uint8_t range_lsb : 4;
   uint8_t dopper_velocity_msb : 4;
-  uint8_t dopper_velocity_msb : 6;
+  uint8_t dopper_velocity_lsb : 6;
   uint8_t bearing_msb : 2;
   uint8_t bearing_lsb : 8;
   uint8_t amplitude : 8;
@@ -40,39 +54,44 @@ typedef struct
 
 typedef struct
 {
-  
+  // TODO
 } MsgTargetCartesian;
 
 typedef struct
 {
-  
+  // TODO
 } MsgTargetCartesianMid;
 
 typedef struct
 {
-  
+  // TODO
 } MsgTargetCartesianMul;
 
 typedef struct
 {
-  
+  // TODO
 } MsgFreespaceSegments;
 
 typedef struct
 {
-  
+  // TODO
 } MsgRawPolarLong;
 
 typedef struct
 {
-  
+  // TODO
 } MsgTargetPolarLong;
 
 enum
 {
-  ID_AUTOLIV_REPORT_1              =   0x380,
-  ID_AUTOLIV_REPORT_2              =   0x381,
-
+  ID_TARGET_POLAR_SHORT     = 0x1,
+  ID_RAW_POLAR_SHORT        = 0x2,
+  ID_TARGET_CARTESIAN       = 0x3,
+  ID_TARGET_CARTESIAN_MID   = 0x4,
+  ID_TARGET_CARTESIAN_MUL   = 0x5,
+  ID_FREESPACE_SEGMENTS     = 0x6,
+  ID_RAW_POLAR_LONG         = 0x7,
+  ID_TARGET_POLAR_LONG      = 0x8
 };
 
 }
